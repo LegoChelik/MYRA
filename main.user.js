ae76363ac8b27692b056293e1f96101afcdf169b
MAZUTA <47991385+LegoChelik@users.noreply.github.com>
2025-02-13 22:53:59 +0300

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Make YouTube Red Again
-// @version      1.2.2
+// @version      1.2.3
 // @description  Removes YouTube's pink accent almost everywhere
 // @author       MAZUTA
 // @downloadURL  https://github.com/LegoChelik/MYRA/raw/main/main.user.js
@@ -24,7 +24,7 @@ yt-icon g path[fill="#FF0033"], yt-icon g circle[fill="#FF0033"], yt-icon path[f
 }
 
 yt-page-navigation-progress[enable-refresh-signature-moments-web] #progress.yt-page-navigation-progress,
-.ytp-cairo-refresh-signature-moments .ytp-play-progress,
+.ytp-play-progress,
 .ytp-cairo-refresh .ytp-swatch-background-color,
 ytd-thumbnail-overlay-resume-playback-renderer[enable-refresh-signature-moments-web] #progress.ytd-thumbnail-overlay-resume-playback-renderer,
 .badge-style-type-live-now-alternate.ytd-badge-supported-renderer,
